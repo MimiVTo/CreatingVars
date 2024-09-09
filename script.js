6e6c744244b7fd256bd3d086e3125006500ce825
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2024-09-09 15:28:37 -0700

    Added.

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ var num1 = 3;
 var num2 = 7;
 var sum = num1 + num2;
 
-console.log(sum)
+console.log(sum);
+
+//Practice
+
+var randomNum = 32;
+
+if(randomNum < 10){
+    console.log("The number is less than 10");
+}
